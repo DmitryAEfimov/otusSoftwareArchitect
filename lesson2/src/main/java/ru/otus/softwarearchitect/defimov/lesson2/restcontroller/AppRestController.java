b27ab0aa560d4d3f31b7b8eb3835a0dbd32f5403
dmitryaefimov <dmitry.a.efimov@gmail.com>
2020-04-11 20:47:50 +0300

    runnable application

---
+++ lesson2/src/main/java/ru/otus/softwarearchitect/defimov/lesson2/restcontroller/AppRestController.java
@@ -0,0 +1,24 @@
+package ru.otus.softwarearchitect.defimov.lesson2.restcontroller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppRestController {
+	private final ObjectMapper mapper;
+
+	public AppRestController(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
+
+	@GetMapping(value = "health")
+	public ObjectNode checkHealth() {
+		ObjectNode response = mapper.createObjectNode();
+		response.put("status", HttpStatus.OK.value());
+
+		return response;
+	}
+}

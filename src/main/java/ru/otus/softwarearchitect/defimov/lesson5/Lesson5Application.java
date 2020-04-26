cd7c69143f3d14fd8a3e4933f32cbab09c37ad3f
DmitryAEfimov <dmitry.a.efimov@gmail.com>
2020-04-26 03:56:05 +0300

    error handling in application

--- src/main/java/ru/otus/softwarearchitect/defimov/lesson5/Lesson5Application.java
+++ src/main/java/ru/otus/softwarearchitect/defimov/lesson5/Lesson5Application.java
@@ -2,11 +2,28 @@ package ru.otus.softwarearchitect.defimov.lesson5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 
 @SpringBootApplication
 public class Lesson5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lesson5Application.class, args);
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+		messageSource.setBasenames("classpath:errorMessages");
+		messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+		messageSource.setDefaultLocale(Locale.US);
+
+		return messageSource;
 	}
 }
--- src/main/java/ru/otus/softwarearchitect/defimov/lesson5/restcontroller/AppRestController.java
+++ src/main/java/ru/otus/softwarearchitect/defimov/lesson5/restcontroller/AppRestController.java
@@ -1,7 +1,7 @@
 package ru.otus.softwarearchitect.defimov.lesson5.restcontroller;
 
+import org.springframework.context.MessageSource;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.softwarearchitect.defimov.lesson5.model.User;
@@ -10,22 +10,31 @@ import ru.otus.softwarearchitect.defimov.lesson5.restcontroller.exception.UserCh
 import ru.otus.softwarearchitect.defimov.lesson5.restcontroller.exception.UserNotFoundException;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 @RestController
 public class AppRestController {
 	private final UserRepository userRepository;
+	private MessageSource messageSource;
 
-	public AppRestController(UserRepository userRepository) {
+	public AppRestController(UserRepository userRepository, MessageSource messageSource) {
 		this.userRepository = userRepository;
+		this.messageSource = messageSource;
 	}
 
 	@PostMapping(value = "users")
 	public User createUser(@RequestBody User user) {
 		try {
+			checkArgument(user.getId() == null);
+
 			return userRepository.save(user);
 		} catch (DataIntegrityViolationException ex) {
 			throw new UserChangeException(Optional.ofNullable(ex.getRootCause()).orElse(ex).getMessage());
+		} catch (IllegalArgumentException ex) {
+			throw new UserChangeException(messageSource.getMessage("userIdAlreadyExists", null, Locale.US));
 		}
 	}
 
@@ -41,6 +50,17 @@ public class AppRestController {
 		}
 	}
 
+	@DeleteMapping(value = "users", params = {"id"})
+	public void deleteUser(@RequestParam(name = "id") int id) {
+		User user = findById(id);
+
+		try {
+			userRepository.delete(user);
+		} catch (DataIntegrityViolationException ex) {
+			throw new UserChangeException(Optional.ofNullable(ex.getRootCause()).orElse(ex).getMessage());
+		}
+	}
+
 	@GetMapping(value = "users", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<User> findAll() {
 		return userRepository.findAll();
@@ -51,16 +71,7 @@ public class AppRestController {
 		return findById(id);
 	}
 
-	@DeleteMapping(value = "users", params = {"id"})
-	public void deleteUser(@RequestParam(name = "id") int id) {
-		try {
-			userRepository.deleteById(id);
-		} catch (DataIntegrityViolationException | EmptyResultDataAccessException ex) {
-			throw new UserChangeException(Optional.ofNullable(ex.getRootCause()).orElse(ex).getMessage());
-		}
-	}
-
 	private User findById(int id) {
-		return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("id=" + id));
+		return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(messageSource.getMessage("userNotFound", new Object[]{id}, Locale.US)));
 	}
 }

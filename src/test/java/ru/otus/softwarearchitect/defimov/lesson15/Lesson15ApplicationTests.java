01330ca8c7e7b4b3e24bb08c91aa6ebe69e3fe12
Dmitry Efimov <dmitry.a.efimov@gmail.com>
2020-07-03 23:02:51 +0300

    adjust test config class name

--- src/test/java/ru/otus/softwarearchitect/defimov/lesson15/Lesson15ApplicationTests.java
+++ src/test/java/ru/otus/softwarearchitect/defimov/lesson15/Lesson15ApplicationTests.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lesson7ApplicationTests {
-
+class Lesson15ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
